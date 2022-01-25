e70f790e5d3656ced128f34053cc937b637da571
Frederico Camara <frederico.camara@serpro.gov.br>
2022-01-25 17:02:47 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -5,10 +5,10 @@ require 'redmine_openid_connect/hooks'
 
 Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
-  author 'Alfonso Juan Dillera / Markus M. May'
-  description 'OpenID Connect implementation for Redmine'
-  version '0.9.2'
-  url 'https://github.com/devopskube/redmine_git_remote'
+  author 'Alfonso Juan Dillera / Markus M. May / Frederico Camara'
+  description 'Serpro OpenID Connect implementation for Redmine'
+  version '0.10.0'
+  url 'https://github.com/fredsdc/redmine_git_remote'
   author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
