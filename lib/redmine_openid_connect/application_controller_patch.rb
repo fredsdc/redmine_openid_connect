ed1b002d44fe85ce59f181f69638489a76ae6ca9
Frederico Camara <frederico.camara@serpro.gov.br>
2022-02-03 16:54:43 -0300

    Allow api_request with local login for authenticated users

--- lib/redmine_openid_connect/application_controller_patch.rb
+++ lib/redmine_openid_connect/application_controller_patch.rb
@@ -12,5 +12,13 @@ module RedmineOpenidConnect
       super(user)
       session[:oic_session_id] = oic_session_id
     end
+
+    def find_current_user
+      if api_request? && Setting.rest_api_enabled? && accept_api_auth? && !api_key_from_request.present? &&
+         session[:user_id] &&
+        params[:key] = User.active.find(session[:user_id]).api_key rescue nil
+      end
+      user = super
+    end
   end
 end
